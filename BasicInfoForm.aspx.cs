2899528ca3f471d8c7864d80f1f76624c89eef23
emilymhoang <50003047+emilymhoang@users.noreply.github.com>
2019-10-27 14:30:21 -0400

    Update BasicInfoForm.aspx.cs

--- BasicInfoForm.aspx.cs
+++ BasicInfoForm.aspx.cs
@@ -1,12 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class BasicInfoForm : System.Web.UI.Page
 {
+    SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["CapstoneConnectionString"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -14,7 +17,13 @@ public partial class BasicInfoForm : System.Web.UI.Page
 
     protected void submitBasicInfo(object sender, EventArgs e)
     {
-        firstNameTextbox.Text = "hello";
+        //sc.Open();
+        //System.Data.SqlClient.SqlCommand insert = new System.Data.SqlClient.SqlCommand();
+        //insert.Connection = sc;
+
+        //create a Tenant object
+        Tenant newTenantInfo = new Tenant(firstNameTextbox.Text, lastNameTextbox.Text, genderTextbox.Text,
+            dateOfBirthTextbox.Text, emailTextbox.Text, phoneNumberTextbox.Text);
     }
 
 }
